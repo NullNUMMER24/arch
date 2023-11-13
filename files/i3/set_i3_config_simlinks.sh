a728d5b4de5c435b0aafb433beb863f79b49cb61
nullnummer24 <j.rohrbach@sensemail.ch>
2023-11-13 15:13:56 +0100

    update

--- files/i3/set_i3_config_simlinks.sh
+++ files/i3/set_i3_config_simlinks.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ln -sf ~/.config/i3/config1 ~/.config/i3/config
-ln -sf ~/.config/i3/config2 ~/.config/i3/config_alt
+ln -sf ~/.config/i3/configHome ~/.config/i3/config
+ln -sf ~/.config/i3/configWork ~/.config/i3/config_alt
+ln -sf ~/.config/i3/config2 ~/.config/i3/config_alt_alt
