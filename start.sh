47bb706b499e15183c48cb1f570346077b2b7104
Rohrbach Jamie <jamie.rohrbach@bedag.ch>
2024-01-26 11:38:46 +0100

    update start script

--- start.sh
+++ start.sh
@@ -1,7 +1,68 @@
-#!bin/bash
+#!/bin/bash
+
+DOTFILES_DIR="$HOME/.dotfiles"
 
 # Create .dotfiles folder
-mkdir -p ~/.dotfiles
+mkdir -p "$DOTFILES_DIR"
 
 # Copy files to .dotfiles folder
-cp -r ../arch/* ~/.dotfiles
+#cp -r ../arch/* "$DOTFILES_DIR"
+
+# List with programs to install
+PROGRAMS=(
+    "discord"
+    "obsidian"
+    "keepassxc"
+    "neovim"
+    #"program"
+)
+
+YUBIKEY_PROGRAMS=(
+    "yubico-authenticator-bin"
+    "yubikey-manager"
+    "yubikey-personalization-gui"
+)
+
+FONTS=(
+    "ttf-firacode"
+    "ttf-firacode-nerd"
+)
+
+VirtualManager=(
+    "qemu"
+    "virt-manager"
+    "virt-viewer"
+    "dnsmasq"
+    "vde2"
+    "bridge-utils"
+    "openbsd-netcat"
+    "dmidecode"
+    "libguestfs"
+)
+
+install_PROGRAMS() {
+    for program in "${PROGRAMS[@]}"; do
+        yay --noconfirm -S "$program" >/dev/null
+    done
+}
+
+install_YUBIKEY() {
+    for program in "${YUBIKEY_PROGRAMS[@]}"; do
+        yay --noconfirm -S "$program" >/dev/null
+    done
+}
+
+install_FONTS() {
+    for font in "${FONTS[@]}"; do
+        yay --noconfirm -S "$font" >/dev/null
+    done
+}
+
+install_KVM() {
+    for program in "${VirtualManager[@]}"; do
+        yay --noconfirm -S "$program" >/dev/null
+    done
+    sudo systemctl enable --now libvirtd
+}
+
+install_PROGRAMS
